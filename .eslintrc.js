a68f9dc050dde48750797281ec306a95d22de436
OKAUEND <def1f23sbb@gmail.com>
2019-10-04 01:46:56 +0900

    OTHER - 開発環境ファイルの設定

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/essential",
+    "prettier",
+    "plugin:prettier/recommended",
+    "@vue/prettier",
+  ],
+  plugins: ["prettier"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/html-indent": ["error", 2],
+    "prettier/prettier": [
+      "error",
+      {
+        trailingComma: "es5",
+      },
+    ],
+  },
+  parserOptions: {
+    parser: "babel-eslint",
+  },
+  overrides: [
+    {
+      files: ["**/__tests__/*.{j,t}s?(x)"],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
