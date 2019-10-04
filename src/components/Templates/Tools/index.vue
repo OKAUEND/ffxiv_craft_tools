71ad10107605f466c1d2a8b9e64dc9f880ad42f0
OKAUEND <def1f23sbb@gmail.com>
2019-10-04 11:29:20 +0900

    ADD - 画面全体のレイアウトの基本構造を作成 DOMは後にコンポーネントに置き換えるが今は仮置で

--- src/components/Templates/Tools/index.vue
+++ src/components/Templates/Tools/index.vue
@@ -1,4 +1,11 @@
 <template>
+  <div class="wrapper">
+    <div class="sub_header"></div>
+    <div class="main" v-bind:class="isNarrow"></div>
+    <div class="side_detail" v-bind:class="isWide">
+      <button v-on:click="switchDisplayRegion">テスト</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +15,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.wrapper {
+  height: 100%;
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-template-rows: 40px 1fr 1fr;
+}
+  .main {
+    width: 80vw;
+    grid-column: 1;
+    grid-row: 2 / 4;
+    transition: 0.5s;
+}
+
+  .side_detail {
+    grid-column: 2;
+    grid-row: 2 / 4;
+  }
 </style>
