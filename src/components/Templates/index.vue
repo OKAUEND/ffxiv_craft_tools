3b11acdad5560863edb89361fc733638b16594d5
OKAUEND <def1f23sbb@gmail.com>
2019-10-05 02:48:13 +0900

    ADD - レイアウトを作成 後のコンポーネントに置き換えるため現在は仮置

--- src/components/Templates/index.vue
+++ src/components/Templates/index.vue
@@ -1,4 +1,9 @@
 <template>
+  <div class="grid_container">
+    <div class="header"></div>
+    <tools-index class="main"></tools-index>
+    <div class="footer"></div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +13,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.grid_container {
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 50px 1fr 4fr 30px;
+}
+
+.header {
+  width: 100%;
+  grid-column: 1;
+  grid-row: 1;
+}
+
+.main {
+  width: 100%;
+  grid-column: 1;
+  grid-row: 2 / 4;
+}
 </style>
