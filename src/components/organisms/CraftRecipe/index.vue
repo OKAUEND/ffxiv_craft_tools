494b53c37f125f0a1757c9c0f7389751fdb54466
OKAUEND <def1f23sbb@gmail.com>
2019-10-07 00:49:17 +0900

    ADD - コンポーネントのDOMの構成を作成 後に更に下位に切り出す予定

--- src/components/organisms/CraftRecipe/index.vue
+++ src/components/organisms/CraftRecipe/index.vue
@@ -1,4 +1,17 @@
-<template></template>
+<template>
+  <article class="Recipewarapper">
+    <section class="Recipe-container">
+      <section class="Expansion"></section>
+      <section class="ChoiceDetail">
+        <div class="job filter-container"></div>
+        <div class="Equippedjob filter-container"></div>
+        <div class="level filter-container"></div>
+        <div class="Secretbook filter-container"></div>
+      </section>
+    </section>
+    <section class="RecipeLists"></section>
+  </article>
+</template>
 
 <script>
 export default {
