b2e37606d8f1bf889135758dc9fad22362b9a133
OKAUEND <def1f23sbb@gmail.com>
2019-10-10 09:49:31 +0900

    OTHER - 誤字脱字の修正

--- tests/unit/molecules/CategoriesList/Categories.spec.js
+++ tests/unit/molecules/CategoriesList/Categories.spec.js
@@ -4,5 +4,5 @@ import CategoriesList from "@/components/molecules/CategoriesList/index.vue";
 describe("CategoriesList", () => {
   it("Propでデータを受け取れているか", () => {});
   it("H2タグにタイトルが表示されているか", () => {});
-  it("propで渡されたデータ文リストレンダリングが行われているか", () => {});
+  it("propで渡されたデータ分のリストレンダリングが行われているか", () => {});
 });
