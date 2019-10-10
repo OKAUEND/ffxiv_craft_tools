bc09ac1eb051cf226df43a1d48655856fe79e65d
OKAUEND <def1f23sbb@gmail.com>
2019-10-10 09:30:24 +0900

    TEST - テスト要件を作成

--- tests/unit/molecules/CategoriesList/Categories.spec.js
+++ tests/unit/molecules/CategoriesList/Categories.spec.js
@@ -2,4 +2,7 @@ import { shallowMount } from "@vue/test-utils";
 import CategoriesList from "@/components/molecules/CategoriesList/index.vue";
 
 describe("CategoriesList", () => {
+  it("Propでデータを受け取れているか", () => {});
+  it("H2タグにタイトルが表示されているか", () => {});
+  it("propで渡されたデータ文リストレンダリングが行われているか", () => {});
 });
