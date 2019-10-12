b9c13e5a034001f1db3ed05682b6bb8843a87e0a
OKAUEND <def1f23sbb@gmail.com>
2019-10-12 22:37:01 +0900

    TEST - テスト用のデータを作成

--- tests/unit/organisms/CraftRecipe/CraftRecipe.spec.js
+++ tests/unit/organisms/CraftRecipe/CraftRecipe.spec.js
@@ -1,4 +1,28 @@
 import { shallowMount } from "@vue/test-utils";
 import CategoriesList from "@/components/organisms/CraftRecipe/index.vue";
 
+const Level = {
+  name: "LEVEL",
+  type: "Level",
+  List: [
+    { lowerLevel: 1, upperLevel: 10 },
+    { lowerLevel: 11, upperLevel: 20 },
+    { lowerLevel: 21, upperLevel: 30 },
+    { lowerLevel: 31, upperLevel: 40 },
+    { lowerLevel: 41, upperLevel: 50 }
+  ]
+};
+
+const other = {
+  name: "TYPE",
+  type: "other",
+  list: [
+    { name: "戦闘", ID: 1 },
+    { name: "戦闘", ID: 2 },
+    { name: "戦闘", ID: 3 },
+    { name: "戦闘", ID: 4 },
+    { name: "戦闘", ID: 5 }
+  ]
+};
+
 describe("CraftRecipe", () => {});
