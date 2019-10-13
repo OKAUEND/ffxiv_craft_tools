991de8e6fb00e6e3f483e79dfc834a737b9f0556
OKAUEND <def1f23sbb@gmail.com>
2019-10-14 02:26:44 +0900

    TEST - 実装するテストが別コンポーネントのだったため、内容を初期化

--- tests/unit/organisms/CraftRecipe/CraftRecipe.spec.js
+++ tests/unit/organisms/CraftRecipe/CraftRecipe.spec.js
@@ -1,35 +1,4 @@
 import { shallowMount } from "@vue/test-utils";
 import CategoriesList from "@/components/organisms/CraftRecipe/index.vue";
 
-const Level = {
-  name: "LEVEL",
-  type: "Level",
-  List: [
-    { ID: 1, lowerLevel: 1, upperLevel: 10 },
-    { ID: 2, lowerLevel: 11, upperLevel: 20 },
-    { ID: 3, lowerLevel: 21, upperLevel: 30 },
-    { ID: 4, lowerLevel: 31, upperLevel: 40 },
-    { ID: 5, lowerLevel: 41, upperLevel: 50 }
-  ]
-};
-
-const other = {
-  name: "TYPE",
-  type: "other",
-  list: [
-    { name: "戦闘", ID: 1 },
-    { name: "非戦闘", ID: 2 },
-    { name: "おしゃれ", ID: 3 },
-    { name: "家具", ID: 4 },
-    { name: "庭具", ID: 5 }
-  ]
-};
-
-describe("CraftRecipe", () => {
-  it("propでデータを渡されるか", () => {});
-  it("H2に文字が表示されるか", () => {});
-  it("typeがLEVELの場合、リストレンダリングされているか", () => {});
-  it("typeがLEVELの場合、下限Levelと上限Levelを文字列結合して文字列が表示されてるか", () => {});
-  it("typeがotherの場合、リストレンダリングされているか", () => {});
-  it("typeがotherの場合、nameの文字列が表示されてるか", () => {});
-});
+describe("CraftRecipe", () => {});
