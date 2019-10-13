f071d7812fcf8f563bd81779fe2b853a6d692fb2
OKAUEND <def1f23sbb@gmail.com>
2019-10-14 02:27:02 +0900

    TEST - テスト用データを作成

--- tests/unit/molecules/CategoriesList/Categories.spec.js
+++ tests/unit/molecules/CategoriesList/Categories.spec.js
@@ -1,6 +1,29 @@
 import { shallowMount } from "@vue/test-utils";
 import CategoriesList from "@/components/molecules/CategoriesList/index.vue";
 
+const Level = {
+  name: "LEVEL",
+  type: "Level",
+  List: [
+    { ID: 1, lowerLevel: 1, upperLevel: 10 },
+    { ID: 2, lowerLevel: 11, upperLevel: 20 },
+    { ID: 3, lowerLevel: 21, upperLevel: 30 },
+    { ID: 4, lowerLevel: 31, upperLevel: 40 },
+    { ID: 5, lowerLevel: 41, upperLevel: 50 }
+  ]
+};
+
+const other = {
+  name: "TYPE",
+  type: "other",
+  list: [
+    { name: "戦闘", ID: 1 },
+    { name: "非戦闘", ID: 2 },
+    { name: "おしゃれ", ID: 3 },
+    { name: "家具", ID: 4 },
+    { name: "庭具", ID: 5 }
+  ]
+};
 describe("CategoriesList", () => {
   it("Propでデータを受け取れているか", () => {});
   it("H2タグにタイトルが表示されているか", () => {});
