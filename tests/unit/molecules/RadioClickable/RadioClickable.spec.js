64e7031a7609faaf509a4ae87550501e9955ca44
OKAUEND <def1f23sbb@gmail.com>
2019-10-14 12:38:02 +0900

    TEST - テスト用データを作成

--- tests/unit/molecules/RadioClickable/RadioClickable.spec.js
+++ tests/unit/molecules/RadioClickable/RadioClickable.spec.js
@@ -1,4 +1,11 @@
 import { shallowMount } from "@vue/test-utils";
 import RadioClickable from "@/components/molecules/RadioClickable/index.vue";
 
-describe("RadioClickable", () => {});
+const LevelType = "Level";
+const OtherType = "other";
+
+const LevelData = { ID: 1, lowerLevel: 1, upperLevel: 10 };
+const OtherData = { ID: 1, lowerLevel: 1, upperLevel: 10 };
+
+describe("RadioClickable", () => {
+});
