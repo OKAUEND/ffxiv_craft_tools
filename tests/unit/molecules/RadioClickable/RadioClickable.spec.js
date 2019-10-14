4262d2e9ad8600c65a5c14fa0f01e93218a1d38f
OKAUEND <def1f23sbb@gmail.com>
2019-10-14 15:23:14 +0900

    TEST - 単体テストを実装

--- tests/unit/molecules/RadioClickable/RadioClickable.spec.js
+++ tests/unit/molecules/RadioClickable/RadioClickable.spec.js
@@ -8,5 +8,23 @@ const LevelData = { ID: 1, lowerLevel: 1, upperLevel: 10 };
 const OtherData = { ID: 1, name: "戦闘" };
 
 describe("RadioClickable", () => {
-  it("propのTypeがLevelのときに、下限レベルと上限レベルを文字列結合して表示しているか", () => {});
+  it("propのTypeがLevelのときに、下限レベルと上限レベルを文字列結合して表示しているか", () => {
+    const Warapper = shallowMount(RadioClickable, {
+      propsData: {
+        typecode: LevelType,
+        value: LevelData
+      }
+    });
+    const LabelText = LevelData.lowerLevel + "~" + LevelData.upperLevel;
+    expect(Warapper.find(".button-design").text()).toBe(LabelText);
+  });
+  it("propのTypeがotherのときに、nameの値を表示できるか", () => {
+    const Warapper = shallowMount(RadioClickable, {
+      propsData: {
+        typecode: OtherType,
+        value: OtherData
+      }
+    });
+    expect(Warapper.find(".button-design").text()).toBe(OtherData.name);
+  });
 });
