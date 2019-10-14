15da56e4addcdb832e76d2180f1d2532722778ca
OKAUEND <def1f23sbb@gmail.com>
2019-10-14 15:21:31 +0900

    TEST - テスト用データの修正

--- tests/unit/molecules/RadioClickable/RadioClickable.spec.js
+++ tests/unit/molecules/RadioClickable/RadioClickable.spec.js
@@ -5,7 +5,7 @@ const LevelType = "Level";
 const OtherType = "other";
 
 const LevelData = { ID: 1, lowerLevel: 1, upperLevel: 10 };
-const OtherData = { ID: 1, lowerLevel: 1, upperLevel: 10 };
+const OtherData = { ID: 1, name: "戦闘" };
 
 describe("RadioClickable", () => {
   it("propのTypeがLevelのときに、下限レベルと上限レベルを文字列結合して表示しているか", () => {});
