7dc810198eae4c5ed8a851dda4aa08f9ae712483
OKAUEND <def1f23sbb@gmail.com>
2019-10-17 16:38:58 +0900

    ADD - HTML要素を構成

--- src/components/molecules/RadioButtomLabel/index.vue
+++ src/components/molecules/RadioButtomLabel/index.vue
@@ -1,4 +1,8 @@
 <template>
+  <div class="button-design">
+    <input type="radio" v-bind:id="createLabelForTags" />
+    <label v-bind:for="createLabelForTags">{{ LabelText }}</label>
+  </div>
 </template>
 
 <script>
