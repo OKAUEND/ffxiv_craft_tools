7646e8135dce5c5205332de9e86ff836b732564f
OKAUEND <def1f23sbb@gmail.com>
2019-10-20 00:40:16 +0900

    ADD - 拡張パック毎にクラスを作成 Styleはのちほど指定する

--- src/components/molecules/ExpansionList/ExpansionState.js
+++ src/components/molecules/ExpansionList/ExpansionState.js
@@ -1,11 +1,51 @@
 export class ARealReborn {
+  constructor(ExpansionName) {
+    this.ExpansionName = ExpansionName;
+  }
+  static create() {
+    return new ARealReborn();
+  }
+
+  get style() {
+    return {};
+  }
 }
 
 export class Heavensward {
+  constructor(ExpansionName) {
+    this.ExpansionName = ExpansionName;
+  }
+  static create() {
+    return new Heavensward();
+  }
+
+  get style() {
+    return {};
+  }
 }
 
 export class Stormblood {
+  constructor(ExpansionName) {
+    this.ExpansionName = ExpansionName;
+  }
+  static create() {
+    return new Stormblood();
+  }
+
+  get style() {
+    return {};
+  }
 }
 
 export class Shadowbringers {
+  constructor(ExpansionName) {
+    this.ExpansionName = ExpansionName;
+  }
+  static create() {
+    return new Shadowbringers();
+  }
+
+  get style() {
+    return {};
+  }
 }
