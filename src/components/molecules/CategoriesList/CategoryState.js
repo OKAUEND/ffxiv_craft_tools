847b415fe94929afb5ceb3ea25280684b43970ca
OKAUEND <def1f23sbb@gmail.com>
2019-10-22 12:22:10 +0900

    ADD - クラスの中身を作成 量が少ないので、一括でコミット

--- src/components/molecules/CategoriesList/CategoryState.js
+++ src/components/molecules/CategoriesList/CategoryState.js
@@ -1,11 +1,36 @@
 class Category {
+  constructor(value) {
+    this.category = value;
+}
 }
 
 export class IconState extends Category {
+  static create(value) {
+    return new IconState(value);
+}
+
+  get Label() {
+    return this.category.Image;
+}
 }
 
 export class StringState extends Category {
+  static create(value) {
+    return new StringState(value);
+}
+
+  get Label() {
+    return this.category.name;
+  }
 }
 
 export class LevelState extends Category {
+  static create(value) {
+    return new LevelState(value);
+  }
+
+  get Label() {
+    super();
+    return this.category.lowerLevel + "~" + this.category.upperLevel;
+  }
 }
