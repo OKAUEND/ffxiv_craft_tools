0d27fbb6b3fcfc8fc8d75fab3fe79355780ca842
OKAUEND <def1f23sbb@gmail.com>
2019-11-07 10:59:36 +0900

    TEST - テストする対象のオブジェクトを事前に作成

--- tests/unit/molecules/RadioButtomLabel/RadioButtomLabel.spec.js
+++ tests/unit/molecules/RadioButtomLabel/RadioButtomLabel.spec.js
@@ -6,44 +6,22 @@ import {
 } from "@/components/molecules/CategoriesList/CategoryState.js";
 import { LevelArray, Icons } from "@/testData.js";
 
-const LevelType = "Level";
-const OtherType = "Equipment";
+const LevelData = LevelArray[1];
+const ImgsData = Icons[1];
 
-const LevelData = { ID: 1, lowerLevel: 1, upperLevel: 10 };
-const OtherData = { ID: 1, name: "戦闘" };
+const LevelClass = LevelState.create(LevelData);
+const IconClass = JobState.create(ImgsData);
 
-describe("RadioClickable", () => {
-  it("propのTypeがLevelのときに、下限レベルと上限レベルを文字列結合して表示しているか", () => {
-    const Warapper = shallowMount(RadioButtomLabel, {
-      propsData: {
-        typecode: LevelType,
-        value: LevelData
-      }
-    });
-    const LabelText = LevelData.lowerLevel + "~" + LevelData.upperLevel;
-    expect(Warapper.find(".button-design").text()).toBe(LabelText);
-  });
-  it("propのTypeがotherのときに、nameの値を表示できるか", () => {
-    const Warapper = shallowMount(RadioButtomLabel, {
-      propsData: {
-        typecode: OtherType,
-        value: OtherData
-      }
-    });
-    expect(Warapper.find(".button-design").text()).toBe(OtherData.name);
-  });
-  it("INPUTのIDタグとLabelのforタグが同じ値で紐付けられるか", () => {
-    const Warapper = shallowMount(RadioButtomLabel, {
-      propsData: {
-        typecode: LevelType,
-        value: LevelData
-      }
-    });
-    const BindTags = LevelType + LevelData.ID;
-    expect(Warapper.find("input").attributes().id).toBe(BindTags);
-    expect(Warapper.find("label").attributes().for).toBe(BindTags);
-    expect(Warapper.find("input").attributes().id).toBe(
-      Warapper.find("label").attributes().for
-    );
-  });
+const LevelWarapper = shallowMount(RadioButtomLabel, {
+  propsData: {
+    ClassStateObject: LevelClass,
+    value: ""
+  }
+});
+
+const IconWarapper = shallowMount(RadioButtomLabel, {
+  propsData: {
+    ClassStateObject: IconClass,
+    value: ""
+  }
 });
