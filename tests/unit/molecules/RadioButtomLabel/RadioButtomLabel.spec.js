12266c92ebc0ebfde70a502e0ed4b17c783beb61
OKAUEND <def1f23sbb@gmail.com>
2019-11-07 10:59:51 +0900

    TEST - 各種テストを実装

--- tests/unit/molecules/RadioButtomLabel/RadioButtomLabel.spec.js
+++ tests/unit/molecules/RadioButtomLabel/RadioButtomLabel.spec.js
@@ -25,3 +25,32 @@ const IconWarapper = shallowMount(RadioButtomLabel, {
     value: ""
   }
 });
+
+describe("RadioClickable", () => {
+  it("propでClassのインスタンスが渡されているか", () => {
+    expect(LevelWarapper.props().ClassStateObject).toBe(LevelClass);
+    expect(IconWarapper.props().ClassStateObject).toBe(IconClass);
+  });
+  it("propで渡されたインスタンスで、TEXTを出力できるか", () => {
+    expect(LevelWarapper.find("label").text()).toBe(LevelClass.Label);
+  });
+  it("INPUTのIDタグとLabelのforタグが同じ値で紐付けられるか", () => {
+    const id = LevelClass.formID;
+    expect(LevelWarapper.find("input").attributes().id).toBe(id);
+    expect(LevelWarapper.find("label").attributes().for).toBe(id);
+    expect(LevelWarapper.find("input").attributes().id).toBe(
+      LevelWarapper.find("label").attributes().for
+    );
+  });
+  it("Iconデータの場合、文字列ではなく画像が作成されているか", () => {
+    expect(IconWarapper.find("img").attributes().src).toBe(IconClass.Label);
+  });
+  it("Radioボタンをクリックした時にemitされるか", () => {
+    const Levelradioinput = LevelWarapper.find('input[type="radio"]');
+    Levelradioinput.trigger("click");
+    expect(LevelWarapper.emitted().change).toBeTruthy();
+    const IconRadioInput = IconWarapper.find('input[type="radio"]');
+    IconRadioInput.trigger("click");
+    expect(IconWarapper.emitted().change).toBeTruthy();
+  });
+});
