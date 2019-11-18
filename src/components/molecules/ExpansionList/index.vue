aef57b21cefa74187f4549a893e071ac87cfafed
OKAUEND <def1f23sbb@gmail.com>
2019-11-18 10:48:54 +0900

    CSS - CSSスタイルを指定

--- src/components/molecules/ExpansionList/index.vue
+++ src/components/molecules/ExpansionList/index.vue
@@ -55,4 +55,44 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+//拡張パックを示すボタンは独自デザインにしたいので、ここで指定する
+.Expansion_area {
+  position: relative;
+  display: inline-block;
+  height: 60px;
+  width: 90px;
+  line-height: 60px;
+  text-align: center;
+  font-size: 25px;
+  color: #faebd7;
+  background-color: #131212;
+  &::after {
+    content: "";
+    display: inline-block;
+    position: absolute;
+    width: 90px;
+    height: 60px;
+    top: 0px;
+    left: 0px;
+    transition: all 300ms 0s ease;
+    opacity: 0;
+  }
+  &._AAR::after {
+    border-left: 5px solid #85d2ff;
+  }
+  &._HEV::after {
+    border-left: 5px solid #325ccf;
+  }
+  &._STB::after {
+    border-left: 5px solid #cf3232;
+  }
+  &._SHB::after {
+    border-left: 5px solid #6c2fce;
+  }
+  &.Expansion_radio--checkmark {
+    &::after {
+      opacity: 1;
+    }
+  }
+}
 </style>
