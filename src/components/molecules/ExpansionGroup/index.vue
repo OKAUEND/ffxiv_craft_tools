ae6946a88e75b019aa49b4bcf8c25ba9b146e77f
OKAUEND <def1f23sbb@gmail.com>
2019-11-18 10:41:11 +0900

    ADD - 拡張パックを選択するボタン郡を括るComponentを作成

---
+++ src/components/molecules/ExpansionGroup/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="Expansion">
+    <expansion-item
+      v-for="(item, key) in Expansions"
+      v-bind:key="key"
+      v-bind:Expansion="item"
+      v-bind:SelectValue="SelectedValue"
+      v-on:change="onExpasionButtomClick"
+      class="Expansion_area"
+    ></expansion-item>
+  </section>
+</template>
+
+<script>
+import ExpansionItem from "@/components/molecules/ExpansionList/index.vue";
+export default {
+  name: "ExpansionGroup",
+  components: {
+    ExpansionItem
+  },
+  props: {
+    Expansions: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      SelectRadioValue: ""
+    };
+  },
+  computed: {
+    SelectedValue: {
+      set(value) {
+        this.SelectRadioValue = value.ExpansionCode;
+      },
+      get() {
+        return this.SelectRadioValue;
+      }
+    }
+  },
+  methods: {
+    onExpasionButtomClick(value) {
+      this.SelectedValue = value;
+      return this.$emit("change", value);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.Expansion {
+  background-color: #202020;
+}
+.Expansion_area {
+  margin-right: 5px;
+}
+</style>
--- src/components/organisms/CraftRecipe/index.vue
+++ src/components/organisms/CraftRecipe/index.vue
@@ -1,5 +1,7 @@
 <template>
   <article class="Recipewarapper">
+    <section class="select-container">
+      <expansion-group v-bind:Expansions="Expansions"></expansion-group>
       <section class="CategoryDetail">
         <category-group v-bind:Categories="CrafterJobs">CLASS</category-group>
         <category-group v-bind:Categories="Equippeds">Equipped</category-group>
@@ -20,9 +22,11 @@
 
 <script>
 import CategoryGroup from "@/components/molecules/CategoryGroup/index.vue";
+import ExpansionGroup from "@/components/molecules/ExpansionGroup/index.vue";
 export default {
   name: "Oraganisms_CraftRecipe",
   components: {
+    ExpansionGroup,
     CategoryGroup
   },
   computed: {
