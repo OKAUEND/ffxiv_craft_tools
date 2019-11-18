e8ba1c71ada331ef1f2b97b005f4efaab330e525
OKAUEND <def1f23sbb@gmail.com>
2019-11-18 10:45:25 +0900

    NEW  - 拡張パックを選択するボタンの状態管理をするComponentのベースを作成

--- src/components/molecules/ExpansionList/index.vue
+++ src/components/molecules/ExpansionList/index.vue
@@ -1,8 +1,34 @@
-<template></template>
+<template>
+  <radio-buttom-label
+    v-on:change="emitFunc"
+  ></radio-buttom-label>
+</template>
 
 <script>
+import RadioButtomLabel from "@/components/molecules/RadioButtomLabel/index.vue";
 export default {
+  name: "ExpansionItem",
+  components: {
+    RadioButtomLabel
+  },
+  props: {
+    Expansion: {
+      type: Object,
+      required: true
+    },
+    SelectValue: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
   name: "ExpansionList"
+  },
+  methods: {
+    emitFunc(value) {
+      return this.$emit("change", value);
+    }
+  }
 };
 </script>
 
