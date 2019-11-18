9d5331194a1b5cfa9209bb936d295a749cfe2ab0
OKAUEND <def1f23sbb@gmail.com>
2019-11-18 09:54:34 +0900

    ADD - 種類を選択するUIの分割を行ったファイル

---
+++ src/components/molecules/CategoryGroup/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <h3><slot></slot></h3>
+    <categories-list
+      v-for="(item, key) in Categories"
+      v-bind:key="key"
+      v-bind:category="item"
+      v-bind:value="SelectedValue"
+      v-on:change="onExpasionButtomClick"
+      class="category_area"
+    ></categories-list>
+  </div>
+</template>
+
+<script>
+import CategoriesList from "@/components/molecules/CategoriesList/index.vue";
+export default {
+  components: {
+    CategoriesList
+  },
+  props: {
+    Categories: {
+      type: Array,
+      required: true
+    },
+    isStoreUse: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      StoreObject: Store.state,
+      SelectRadioValue: ""
+    };
+  },
+  computed: {
+    SelectedValue: {
+      set(value) {
+        if (this.isStoreUse) {
+        } else {
+          this.SelectRadioValue = value.name;
+        }
+      },
+      get() {
+        let returnValue;
+        if (this.isStoreUse) {
+        } else {
+          returnValue = this.SelectRadioValue;
+        }
+        return returnValue;
+      }
+    }
+  },
+  methods: {
+    onExpasionButtomClick(value) {
+      this.SelectedValue = value;
+      return this.$emit("change", value);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+h3 {
+  margin: 0;
+  color: #f8f9fa;
+}
+
+.category_area {
+  margin-right: 5px;
+}
+</style>
