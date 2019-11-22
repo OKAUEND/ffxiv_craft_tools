f8c7f2d97e53eda090e1c50b2f3b9bbc6f3f763f
OKAUEND <def1f23sbb@gmail.com>
2019-11-22 12:39:54 +0900

    TEST - テスト要件を作成

--- tests/unit/molecules/ExpansionList/ExpansionList.spec.js
+++ tests/unit/molecules/ExpansionList/ExpansionList.spec.js
@@ -1,4 +1,10 @@
 import { shallowMount } from "@vue/test-utils";
 import ExpansionList from "@/components/molecules/ExpansionList/index.vue";
 
-describe("ExpansionList", () => {});
+describe("ExpansionList", () => {
+  it("Propsが受け取れているか", () => {});
+  it("算出プロパティでタイプ毎にクラスのインスタンスが作成されているか", () => {});
+  it("子からのemit通知を受け取れているか", () => {});
+  it("状態クラスで作成したクラスオブジェクトがv-bind:classに紐付けられてるか", () => {});
+  it("propsのvalueに値があった場合、クラスが3つになっているか", () => {});
+});
