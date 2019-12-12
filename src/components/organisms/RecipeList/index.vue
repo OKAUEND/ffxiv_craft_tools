b0c5876200e8fa86a7e7aaee583ca534a9ef9eb0
OKAUEND <def1f23sbb@gmail.com>
2019-12-12 16:36:53 +0900

    NEW - バックエンドからデータを取得するのと、データを保持するコンポーネントを作成

---
+++ src/components/organisms/RecipeList/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="RecipeList">
+    <recipe-item></recipe-item>
+  </section>
+</template>
+
+<script>
+import axios from "axios";
+import RecipeItem from "@/components/molecules/RecipeItem/index.vue";
+export default {
+  components: {
+    RecipeItem
+  },
+  data() {
+    return {
+      Recipes: []
+    };
+  },
+  async created() {
+    try {
+    } catch (e) {
+      console.error(e);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.RecipeList {
+  display: flex;
+  align-items: center;
+  flex-flow: column nowrap;
+  width: 100%;
+  padding: 0 10px 0 10px;
+  box-sizing: border-box;
+}
+</style>
