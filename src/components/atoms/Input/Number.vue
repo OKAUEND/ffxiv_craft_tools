9b10de61ae27586c5a3ab80540cf6c95d4f5d0d9
OKAUEND <def1f23sbb@gmail.com>
2019-12-25 12:01:20 +0900

    NEW - 半角数字のみ入力を行えるフォームコンポーネントを作成 Baseをわけておくことで、他のinput部品と構造を共通化させる

---
+++ src/components/atoms/Input/Number.vue
@@ -0,0 +1,35 @@
+<template>
+  <input type="text" :value="propsValue" @input="emitted" />
+</template>
+
+<script>
+import InputBase from "./Base.vue";
+export default {
+  extends: InputBase,
+  name: "InputNumber",
+  methods: {
+    emitted(event) {
+      const value = event.target.value.replace(/[^0-9]/g, "").slice(0, 3);
+      event.target.value = value;
+      this.propsValue = value;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+//後でリセットCSS導入したら消す部分
+input {
+  border: none;
+  margin: 0;
+  padding: 3px 4px 3px 4px;
+  outline: 0;
+}
+//
+.Input-Number {
+  &__Counter {
+    width: 50px;
+    font-size: 20px;
+  }
+}
+</style>
