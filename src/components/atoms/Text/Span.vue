1386378cd6dbcc12491b1600e2bd6e0654836c8a
OKAUEND <def1f23sbb@gmail.com>
2019-12-28 00:45:35 +0900

    NEW - Spanコンポーネントを作成

---
+++ src/components/atoms/Text/Span.vue
@@ -0,0 +1,32 @@
+<template>
+  <span>
+    {{ this.text }}
+  </span>
+</template>
+
+<script>
+import TextBase from "./TextBase.js";
+export default {
+  mixins: [TextBase]
+};
+</script>
+
+<style lang="scss" scoped>
+.Atom-span__Name {
+  &--Large {
+    font-size: 20px;
+    @media screen and(min-width: 481px) {
+      font-size: 25px;
+    }
+  }
+  &--Medium {
+    font-size: 16px;
+    @media screen and(min-width: 481px) {
+      font-size: 20px;
+    }
+  }
+  &--Small {
+    font-size: 15px;
+  }
+}
+</style>
