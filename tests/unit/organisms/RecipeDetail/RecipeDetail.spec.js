469ff6148ea09a7e712d1358f63d16cdf0e89828
OKAUEND <def1f23sbb@gmail.com>
2020-01-04 14:51:51 +0900

    Remove - ファイルをリネームしたため削除

--- tests/unit/organisms/RecipeDetail/RecipeDetail.spec.js
+++
@@ -1,8 +0,0 @@
-import { shallowMount, mount } from "@vue/test-utils";
-import RecipeDetail from "@/components/organisms/RecipeDetail/index.vue";
-
-describe("RecipeDetail", () => {
-  it("axiosを使用しRESTAPIを叩き製作レシピの配列を取得する", () => {});
-  it("APIから取得した配列をフィルタリングしない", () => {});
-  it("Categoryコンポーネントで選択された内容を元に配列のフィルタリングを行う", () => {});
-});
