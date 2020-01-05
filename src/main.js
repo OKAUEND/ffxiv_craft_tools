feb4b5b27bb2350d7c632d7a0f6ef38b62fba359
OKAUEND <def1f23sbb@gmail.com>
2020-01-05 10:16:56 +0900

    ADD - レンダリングするファイルをPageコンポーネントに変更

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
 import router from "./router.js";
-import App from "./App.vue";
+import Pages from "@/components/Pages/index.vue";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(Pages)
 }).$mount("#app");
