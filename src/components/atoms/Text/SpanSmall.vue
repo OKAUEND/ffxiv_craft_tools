f9dbbb0a8af6bff16c83f99e99d1a56d48447207
OKAUEND <def1f23sbb@gmail.com>
2020-01-17 02:19:04 +0900

    NEW - spanコンポーネントを分割

---
+++ src/components/atoms/Text/SpanSmall.vue
@@ -0,0 +1,19 @@
+<template>
+  <span class="Atom__Span--Small">
+    <slot></slot>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "SpanSmall"
+};
+</script>
+
+<style lang="scss" scoped>
+.Atom__Span--Small {
+  width: 100%;
+  display: inline-block;
+  font-size: $font_Small;
+}
+</style>
