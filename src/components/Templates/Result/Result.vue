d7adb204cbd07ebdc3668da37ad1dda1f5e4be67
OKAUEND <def1f23sbb@gmail.com>
2020-01-19 03:01:56 +0900

    NEW - 選択した製作レシピなどの情報を集計した結果などを表示するコンポーネントの親を作成

---
+++ src/components/Templates/Result/Result.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="Result">
+    <nav class="Result__Nav">
+      <atom-image></atom-image>
+      <atom-image> </atom-image>
+    </nav>
+    <div class="Result__Body"></div>
+  </div>
+</template>
+
+<script>
+import AtomImage from "@/components/atoms/Image/ImageMedium.vue";
+export default {
+  name: "ResultIndex",
+  components: {
+    AtomImage
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.Result {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+
+  &__Nav {
+    height: 60px;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+    box-shadow: 1px $Palette_black;
+  }
+
+  &__Body {
+    margin-top: 20px;
+    height: 100%;
+    width: 100%;
+  }
+}
+</style>
