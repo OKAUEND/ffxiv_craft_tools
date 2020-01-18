ecda8ea4f772af029b121fa9dc0f396d9c4bce8d
OKAUEND <def1f23sbb@gmail.com>
2020-01-19 05:41:47 +0900

    NEW - 集計結果を表示するコンポーネントを作成 全部を一括で表示するか、レシピ毎に個別で表示するかで使い分けるために、2つを作成

---
+++ src/components/organisms/ResultDetail/IndividualDetail.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="ResultIndividual">
+    <div class="Result__Item">
+      <atom-image> </atom-image> <atom-span>:10</atom-span>
+    </div>
+    <div class="Result__Item">
+      <atom-image> </atom-image> <atom-span>:10</atom-span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndividualDetail"
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/organisms/ResultDetail/WholeDetail.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="ResultWhole">
+    <div class="Result__Item">
+      <atom-image> </atom-image> <atom-span>:10</atom-span>
+    </div>
+    <div class="Result__Item">
+      <atom-image> </atom-image> <atom-span>:10</atom-span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WholeDetail.vue"
+};
+</script>
+
+<style lang="scss" scoped></style>
