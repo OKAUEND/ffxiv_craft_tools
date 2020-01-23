d8997941cf8aa96d1db79c9aacae23cd5d52574a
OKAUEND <def1f23sbb@gmail.com>
2020-01-23 10:44:46 +0900

    ADD - 権利表記を指定の場所に表記するため、footerを作成 コンポーネントなどで入れ替えを想定しないため、この部分で表記

--- src/components/Templates/index.vue
+++ src/components/Templates/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="Container">
     <header-component class="header"></header-component>
-    <tools-index class="main"></tools-index>
+    <article class="main">
+    </article>
+    <article class="footer">
+      記載されている会社名・製品名・システム名などは、各社の商標、または登録商標です。
+    </article>
   </div>
 </template>
 
@@ -21,8 +25,8 @@ export default {
 .Container {
   display: grid;
   grid-template-columns: 1fr;
-  grid-template-rows: 60px 1fr;
-  background-color: #202020;
+  grid-template-rows: 60px 1fr 30px;
+  background-color: $palette_Dark-gray;
   box-sizing: border-box;
   min-height: 100vh;
 
@@ -42,6 +46,18 @@ export default {
     @media screen and (min-width: 481px) {
       grid-row: 2 / 5;
     }
+  }
+
+  .footer {
+    width: 100%;
+    grid-column: 1;
+    grid-row: 4 / 5;
+    font-size: 11px;
+    color: $palette_Light-gray;
+    text-align: center;
+    padding-left: 5px;
+    padding-right: 5px;
+    box-sizing: border-box;
   }
 }
 </style>
