280248d19ea8f85d9a34be28c340d5dbb06f4541
OKAUEND <def1f23sbb@gmail.com>
2020-01-24 10:30:37 +0900

    ADD - Routingの設定を記述

--- src/router.js
+++ src/router.js
@@ -1,6 +1,43 @@
 import Vue from "vue";
 import Router from "vue-router";
 
+//コンポーネントを読み込む
+import ToolsIndex from "@/components/Templates/Tools/index.vue";
+import AboutIndex from "@/components/Templates/About/About.vue";
+import ResultIndex from "@/components/Templates/Result/Result.vue";
+
+import WholeDetail from "@/components/organisms/ResultDetail/WholeDetail.vue";
+import IndividualDetail from "@/components/organisms/ResultDetail/IndividualDetail.vue";
+
 Vue.use(Router);
 
-export default new Router({});
+export default new Router({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: ToolsIndex
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: AboutIndex
+    },
+    {
+      path: "/result",
+      name: "result",
+      component: ResultIndex,
+      children: [
+        {
+          path: "/",
+          component: WholeDetail
+        },
+        {
+          path: "IndividualDetail",
+          component: IndividualDetail
+        }
+      ]
+    }
+  ]
+});
