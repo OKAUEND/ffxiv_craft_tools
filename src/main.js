7e9860379ab0d243a96e1860dbeb5323dd68ca1f
OKAUEND <def1f23sbb@gmail.com>
2020-10-21 23:14:04 +0900

    Add - 環境更新で消えたSCSSを読み込みコードを再記述

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+require("@/assets/scss/main.scss");
 
 createApp(App)
   .use(store)
