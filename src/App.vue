f39e0ff3358d8a4c3687234581e198f0f4b02e4d
OKAUEND <def1f23sbb@gmail.com>
2020-11-05 09:40:19 +0900

    Change - Vue2版で使っていたAppの内容へ書き換え

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,63 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <article class="Container">
+    <header-component />
+    <article class="main">
+      <router-view />
+    </article>
+    <article class="footer">
+      記載されている会社名・製品名・システム名などは、各社の商標、または登録商標です。
+    </article>
+  </article>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { defineComponent } from "vue";
+import HeaderComponent from "../src/views/header/header.vue";
 
-#nav {
-  padding: 30px;
+export default defineComponent({
+  name: "App",
+  components: { HeaderComponent },
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  setup() {
+    return {};
+  }
+});
+</script>
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<style lang="scss" scoped>
+.Container {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 60px 1fr 20px;
+  background-color: $palette_Dark-gray;
+  box-sizing: border-box;
+  min-height: 100vh;
+
+  > .header {
+    position: sticky;
+    top: 0;
+    grid-column: 1 /2;
+    grid-row: 1 / 2;
+    z-index: 1;
+  }
+
+  > .main {
+    width: 100%;
+    grid-column: 1;
+    grid-row: 2 / 4;
+  }
+
+  .footer {
+    width: 100%;
+    min-height: 20px;
+    grid-column: 1;
+    grid-row: 4 / 5;
+    font-size: 11px;
+    color: $palette_Light-gray;
+    text-align: center;
+    padding-left: 5px;
+    padding-right: 5px;
+    box-sizing: border-box;
   }
 }
 </style>
