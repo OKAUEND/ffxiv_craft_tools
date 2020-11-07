1ba03295117da962d85141437cc2c71b0bb3a631
OKAUEND <def1f23sbb@gmail.com>
2020-11-07 15:38:06 +0900

    TEST - コンポーネントのテスト内容ができたのでコミット

--- tests/unit/Part/CategoryContent/ContentList.spec.ts
+++ tests/unit/Part/CategoryContent/ContentList.spec.ts
@@ -1,8 +1,7 @@
 import { mount } from "@vue/test-utils";
-// import CategoryGroup from "@/components/organisms/CategoryGroup/CategoryGroup.vue";
-// import CategoriesList from "@/components/molecules/CategoriesList/CategoriesList.vue";
 
 import ContentList from "@/components/Parts/CategoryContent/ContentList.vue";
+import ChildComponent from "@/components/Base/Input/BaseInputRadio.vue"
 import crafterJSON from "@/assets/FFXIV.json";
 
 const testData: crafter = crafterJSON.crafter[0];
@@ -31,7 +30,7 @@ const ContentListFactory = (propsData: object) => {
 
 const props: Props = {
   content: testData,
-  value: "",
+  value: "TEST",
 };
 
 describe("ContentList", () => {
@@ -39,31 +38,9 @@ describe("ContentList", () => {
     const wrapper = ContentListFactory(props);
     expect(wrapper.props().content).toStrictEqual(testData);
   });
-  //   it("子からのemitイベントの発火を感知できるか", () => {
-  //     const emitdata = { name: "TEST1" };
-  //     const wrapper = IconFactory();
-  //     wrapper.find(CategoriesList).vm.$emit("change", emitdata);
-  //     expect(wrapper.emitted().change[0][0]).toEqual(emitdata);
-  //   });
-  //   it("受け取ったemitで、Dataのインスタンス変数が変わるか", () => {
-  //     const emitdata = { name: "TEST1" };
-  //     const wrapper = IconFactory();
-  //     wrapper.find(CategoriesList).vm.$emit("change", emitdata);
-  //     expect(wrapper.vm.SelectRadioValue).toEqual(emitdata.name);
-  //   });
-  //   it("親へemitを発火するか", () => {
-  //     const wrapper = IconFactory();
-  //     const emitdata = { name: "TEST1" };
-  //     wrapper.vm.onExpasionButtomClick(emitdata);
-  //     expect(wrapper.emitted().change[0][0]).toBe(emitdata);
-  //   });
-  //   it("HeadTextをクリックしたら真偽値の値が切り替わるか", () => {
-  //     const wrapper = IconFactory();
-  //     expect(wrapper.vm.isLabelClick).toBe(false);
-  //     const HeadText = wrapper.find(".Category__HeadText");
-  //     HeadText.trigger("click");
-  //     expect(wrapper.vm.isLabelClick).toBe(true);
-  //     console.log(wrapper.vm.isLabelClick);
-  //   });
-  //   //   it("LevelとMasterBookの場合、インスタンス変数を使わずにStoreパターンを使うか", () => {});
+    it("子からのemitイベントで親へemitを発火するか", () => {
+      const wrapper = ContentListFactory(props);
+      wrapper.findComponent(ChildComponent).vm.$emit("input");
+      expect(wrapper.emitted().click[0]).toHaveLength(1)
+    });
 });
