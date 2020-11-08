af73caa2ba91ed86512a016743cc4f36413f2dec
OKAUEND <def1f23sbb@gmail.com>
2020-11-08 20:27:06 +0900

    TEST - テストの内容を作成

--- tests/unit/Part/CategoryContent/CategoryContent.spec.ts
+++ tests/unit/Part/CategoryContent/CategoryContent.spec.ts
@@ -1,5 +1,15 @@
 import {} from "@vue/test-utils";
 
-describe("CategoryContent", () => {
+import CategoryContent from "@/components/Parts/CategoryContent/CategoryContent.vue";
+import ChildComponent from "@/components/Parts/CategoryContent/ContentList.vue";
+import crafterJSON from "@/assets/FFXIV.json";
 
+describe("CategoryContent", () => {
+    test("Componentを配列分描画できているか", (): void => {
+
+      });
+
+    test("小からのEmitで、stateが更新されているか", (): void => {
+
+    });
 });
