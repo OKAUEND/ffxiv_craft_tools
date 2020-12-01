9d128b4843eecfe40938b7a22440a214a338ee1e
OKAUEND <def1f23sbb@gmail.com>
2020-12-02 08:36:09 +0900

    Add - テスト項目を追加

--- tests/unit/Pages/LogListContent.spec.ts
+++ tests/unit/Pages/LogListContent.spec.ts
@@ -43,5 +43,30 @@ describe("LogListContent", () => {
         //テストデータは正しく状態を持てている？
         expect(wrapper.vm.selectedcategory["test"]).toEqual({ type: 'test'})
     });
+
+    test("保持する項目は動的であるか", (): void => {
+        const wrapper = LogListContentFactory();
+
+        //初期インスタンス生成時はどの値も保持していないこと
+        expect(wrapper.vm.selectedcategory).toStrictEqual({})
+
+        //テストデータを流し込んでみる
+        wrapper.findComponent(CategoryContent).vm.$emit("update", { type: "test"});
+
+        //テストデータは正しく状態を持てている？
+        expect(wrapper.vm.selectedcategory["test"]).toEqual({ type: 'test'})
+
+        //テストデータを流し込んでみる
+        wrapper.findComponent(CategoryContent).vm.$emit("update", { type: "hoge"});
+
+        //テストデータは正しく状態を持てている？
+        expect(wrapper.vm.selectedcategory["hoge"]).toEqual({ type: 'hoge'})
+
+        //テストデータを流し込んでみる
+        wrapper.findComponent(CategoryContent).vm.$emit("update", { type: "fuga"});
+
+        //テストデータは正しく状態を持てている？
+        expect(wrapper.vm.selectedcategory["fuga"]).toEqual({ type: 'fuga'})
+    });
 });
 
