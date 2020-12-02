5e2323b0f7932cacb8929cf6120307acf8fb6c36
OKAUEND <def1f23sbb@gmail.com>
2020-12-03 06:23:55 +0900

    Add - Firebaseの初期設定を行う ルール等はgithubにあげないように除外しておく

---
+++ src/firebase.ts
@@ -0,0 +1,33 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/storage";
+
+interface FirebaseConfig {
+  apiKey: string;
+  authDomain: string;
+  databaseURL: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+  measurementId: string;
+}
+
+const config: FirebaseConfig = {
+  apiKey: process.env.VUE_APP_apiKey,
+  authDomain: process.env.VUE_APP_authDomain,
+  databaseURL: process.env.VUE_APP_databaseURL,
+  projectId: process.env.VUE_APP_projectId,
+  storageBucket: process.env.VUE_APP_storageBucket,
+  messagingSenderId: process.env.VUE_APP_messagingSenderId,
+  appId: process.env.VUE_APP_appId,
+  measurementId: process.env.VUE_APP_measurementId,
+};
+
+console.log(config);
+
+firebase.initializeApp(config);
+firebase.firestore();
+
+export default firebase;
