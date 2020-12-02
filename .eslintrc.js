8e612c9450fd7f8f1358091c273d4478bc49a28d
OKAUEND <def1f23sbb@gmail.com>
2020-12-03 06:24:42 +0900

    Chore - TypeScriptファイルでうまくコード整形が機能していなかったので、環境を見直し

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,16 @@ module.exports = {
   env: {
     node: true,
   },
+  plugins: ["@typescript-eslint"],
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
     "@vue/prettier",
     "@vue/typescript/recommended",
     "@vue/prettier/@typescript-eslint",
+    "prettier/@typescript-eslint",
   ],
   parserOptions: {
     ecmaVersion: 2020,
