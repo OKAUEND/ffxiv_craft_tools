cf44d091199d9cf40faec2efbc67d484020d0ca8
OKAUEND <def1f23sbb@gmail.com>
2020-12-11 13:09:42 +0900

    Add - DOMとCSSのスタイルを仮設定

--- src/components/Parts/LogPanel/LogPanel.vue
+++ src/components/Parts/LogPanel/LogPanel.vue
@@ -1,8 +1,136 @@
-<template> TEST</template>
+<template>
+  <div class="logpanel">
+    <div class="logpanel__leftside">
+      <base-image-medium
+        :src="craftlog.imageurl"
+        :commet="craftlog.text.name"
+      />
+    </div>
+    <div class="logpanel__topview">
+      <span>{{ craftlog.text.name }}</span>
+    </div>
+    <div class="logpanel__childlist">
+      <div
+        v-for="(child, order) in craftlog.childrenlogs"
+        :key="order"
+        class="logpanel__childItems"
+      >
+        <base-image-small :src="child.imageurl" />
+        <img />
+        <span>{{ child.value }}</span>
+      </div>
+    </div>
+    <div class="logpanel__announce">
+      <button>追加</button>
+    </div>
+  </div>
+</template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-export default defineComponent({});
+import { defineComponent, PropType, computed } from "vue";
+import { FirestoreData } from "@/interface/FFXIVLog.ts";
+
+import BaseImageMedium from "@/components/Base/Image/BaseImageMedium.vue";
+import BaseImageSmall from "@/components/Base/Image/BaseImageSmall.vue";
+
+interface Props {
+  craftdata: FirestoreData;
+}
+
+export default defineComponent({
+  name: "LogPanel",
+  components: {
+    BaseImageMedium,
+    BaseImageSmall,
+  },
+  props: {
+    craftdata: {
+      type: Object as PropType<FirestoreData>,
+      required: true,
+    },
+  },
+
+  setup(props: Props) {
+    const craftlog = computed(() => {
+      return props.craftdata;
+    });
+
+    return {
+      craftlog,
+    };
+  },
+});
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.logpanel {
+  display: grid;
+  grid-template-columns: 100px 2fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  height: 160px;
+  width: 100%;
+  background-color: #131212;
+  color: #fffffe;
+  padding: 5px;
+  margin-bottom: 5px;
+  border-radius: 5px;
+  @media screen and (min-width: 481px) {
+    grid-template-columns: 0.5fr 2fr 0.75fr;
+    grid-template-rows: 1.5fr 1fr;
+  }
+  %--flexCenter {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .logpanel__leftside {
+    @extend %--flexCenter;
+    grid-column: 1 / 2;
+    grid-row: 1 / 4;
+    flex-flow: column nowrap;
+    @media screen and (min-width: 481px) {
+      grid-column: 1 / 2;
+      grid-row: 1 / 3;
+    }
+  }
+  .logpanel__topview {
+    @extend %--flexCenter;
+    grid-column: 2 / 3;
+    grid-row: 1 / 2;
+    flex-direction: row;
+    @media screen and (min-width: 481px) {
+      grid-column: 2 / 3;
+      grid-row: 1 / 2;
+    }
+  }
+  .logpanel__childlist {
+    grid-column: 2/3;
+    grid-row: 2/3;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    @media screen and (min-width: 481px) {
+      display: inline-flex;
+      justify-content: center;
+      align-items: center;
+      grid-column: 2/3;
+      grid-row: 2/3;
+      justify-content: flex-start;
+    }
+  }
+  .logpanel__childItems {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    @media screen and (min-width: 481px) {
+      margin: 2px;
+      width: 55px;
+    }
+  }
+  .logpanel__announce {
+    grid-column: 2/3;
+    grid-row: 3/4;
+  }
+}
+</style>
