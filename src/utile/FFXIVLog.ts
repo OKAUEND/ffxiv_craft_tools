c43f62bbabc21f774ebd85a8f6ff9232e446da2d
OKAUEND <def1f23sbb@gmail.com>
2021-01-13 13:27:53 +0900

    New - 空データを使いたいので作成する

---
+++ src/utile/FFXIVLog.ts
@@ -0,0 +1,10 @@
+import { FirestoreData, FirestoreFetchData } from "./FFXIVLogTypes";
+
+export const emptyLog: FirestoreData = {
+  text: {
+    name: "empty",
+    engname: "empty",
+  },
+  imageurl: "empty",
+  patchversion: 999,
+};
