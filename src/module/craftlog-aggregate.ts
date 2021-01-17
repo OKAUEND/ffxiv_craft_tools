639eab33ab91cdd44c120d156caf69aaee6649af
OKAUEND <def1f23sbb@gmail.com>
2021-01-17 16:26:50 +0900

    New - 選択した製作ログより、必要情報をDBから取得するための関数を書く

---
+++ src/module/craftlog-aggregate.ts
@@ -0,0 +1,43 @@
+import { ref } from "vue";
+
+import { StoreLog, ChildLogs } from "@/utile/FFXIVLogTypes";
+import { assertIsDefind } from "@/utile/craftlogutiles";
+
+const progress = ref<number>(0);
+
+export const useProgress = () => {
+  return progress;
+};
+
+export const validChildFactorOnlyEnable = (
+  childlogs: ChildLogs[]
+): ChildLogs[] => {
+  return childlogs.filter((childlog) => childlog.isEnable === true);
+};
+
+const fetchCraftLogFromPath = async (childlogs: ChildLogs[]) => {
+  const validChildFactor = validChildFactorOnlyEnable(childlogs);
+
+  if (validChildFactor.length === 0) {
+    return;
+  }
+
+  const details = await Promise.all(
+    validChildFactor.map(async (child) => {
+      //ここでFirestoreからデータを取得する
+      //Firebaseのクエリを発行
+    })
+  );
+
+  //再帰呼び出しをし、子要素の子要素を取り出す
+};
+
+export const getCraftLogChildLogDetail = async (cartlogs: StoreLog[]) => {
+  const temp = await Promise.all(
+    cartlogs.map(async (craftlog) => {
+      const temp = await fetchCraftLogFromPath(craftlog.log.Childrenlogs);
+
+      //処理が終わったら進捗度を上げる
+    })
+  );
+};
