5d8b0a1bd349f069607dfb9a1f69899cc4547aea
OKAUEND <def1f23sbb@gmail.com>
2021-01-28 10:06:45 +0900

    Change - 共有になっていたので、個別で使用するため宣言場所を移動

--- src/module/statefull.ts
+++ src/module/statefull.ts
@@ -1,11 +1,10 @@
 import { readonly, Ref, ref } from "vue";
 
-const count = ref<number>(1);
-
 const changeCount = (count: Ref<number>) => (value: number) => {
   count.value = value;
 };
 
 export const useCount = () => {
+  const count = ref<number>(1);
   return { count: readonly(count), changeCount: changeCount(count) };
 };
