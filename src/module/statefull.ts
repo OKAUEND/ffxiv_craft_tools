bcf4c9212a2b9781b671214ec3d564412726d67f
OKAUEND <def1f23sbb@gmail.com>
2021-01-29 09:40:43 +0900

    Add - 進捗度として数値を扱うロジックの担当するコードを追加

--- src/module/statefull.ts
+++ src/module/statefull.ts
@@ -8,3 +8,26 @@ export const useCount = () => {
   const count = ref<number>(1);
   return { count: readonly(count), changeCount: changeCount(count) };
 };
+
+const progress = ref<number>(0);
+const onceValue = ref<number>(0);
+
+const forwardProgress = (
+  progress: Ref<number>,
+  onceValue: Ref<number>
+) => () => {
+  progress.value = progress.value + onceValue.value;
+};
+
+const calculateOnceProgressValue = (logCount: number) => {
+  onceValue.value = PROGRESSVALUE_IN_DATA_REQUEST / logCount;
+};
+
+export const useProgress = () => {
+  return {
+    progress: readonly(progress),
+    onceValue: readonly(onceValue),
+    forwardProgress: forwardProgress(progress, onceValue),
+    calculateOnceProgressValue: calculateOnceProgressValue,
+  };
+};
