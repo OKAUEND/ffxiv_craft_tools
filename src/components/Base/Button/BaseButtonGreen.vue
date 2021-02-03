b7dfeb6e9fb6796a947937f144f88255694c9d8a
OKAUEND <def1f23sbb@gmail.com>
2021-02-04 07:43:15 +0900

    Change - 一部基底コンポーネントのコードをVue3対応

--- src/components/Base/Button/BaseButtonGreen.vue
+++ src/components/Base/Button/BaseButtonGreen.vue
@@ -1,18 +1,40 @@
 <template>
-  <button class="Atom__Button--green" @click="onClick">
+  <button class="BaseButtonGreen" @click="onClick" :disabled="isDisable">
     <slot></slot>
   </button>
 </template>
 
-<script>
-import Base from "./Base";
-export default {
-  mixins: [Base],
-};
+<script lang="ts">
+import { computed, defineComponent, SetupContext } from "vue";
+interface Prop {
+  disable: boolean;
+}
+export default defineComponent({
+  props: {
+    disable: {
+      type: Boolean,
+      defalut: true,
+    },
+  },
+  setup(props: Prop, context: SetupContext) {
+    const isDisable = computed(() => {
+      return props.disable;
+    });
+
+    const onClick = () => {
+      context.emit("click");
+    };
+
+    return {
+      isDisable,
+      onClick,
+    };
+  },
+});
 </script>
 
 <style lang="scss" scoped>
-.Atom__Button--green {
+.BaseButtonGreen {
   @include ButtonBaseSize;
   background-color: $palette_bright-green;
   color: $palette_white;
