380ee4f1395e54cadd5a148e0fb364080df399df
OKAUEND <def1f23sbb@gmail.com>
2021-02-04 20:16:03 +0900

    New - 未選択時にエラー画面として出したい表現をコンポーネント化

---
+++ src/components/Pages/ErrorAnnounce/NoSelectedItem.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>製作レシピを選択していません。レシピを選んでください。</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({});
+</script>
+
+<style></style>
