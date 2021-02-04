bf75bceb14667c33851ac7e6fe71f211f20a1a5c
OKAUEND <def1f23sbb@gmail.com>
2021-02-04 20:16:57 +0900

    New - カート欄で表示するようのアイテムパネルを作成

---
+++ src/components/Parts/CartItemPanel/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="cartitempanel">
+    <div class="cartitempanel__leftside">
+      <base-image-small
+        :path="CartItem.log.imageurl"
+        :comment="CartItem.log.text.name"
+      />
+    </div>
+    <div class="cartitempanel__centerbody">
+      <span>{{ CartItem.log.text.name }}</span>
+      <span>{{ CartItem.value }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, PropType, SetupContext } from "vue";
+import { StoreLog } from "@/@types/FFXIVLogTypes";
+
+import BaseImageSmall from "@/components/Base/Image/BaseImageSmall.vue";
+
+interface Props {
+  item: StoreLog;
+}
+
+export default defineComponent({
+  components: {
+    BaseImageSmall,
+  },
+  props: {
+    item: {
+      type: Object as PropType<StoreLog>,
+      required: true,
+    },
+  },
+  setup(props: Props, contex: SetupContext) {
+    const CartItem = computed(() => {
+      console.log(props.item);
+      return props.item;
+    });
+
+    return { CartItem };
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
