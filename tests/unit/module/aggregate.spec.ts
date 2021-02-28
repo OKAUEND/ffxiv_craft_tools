d19f7bb0aa9095ed933af74a5bbad3f4cd3a35d5
OKAUEND <def1f23sbb@gmail.com>
2021-03-01 08:19:07 +0900

    Add - 引数からテストデータを作成する関数を作成

--- tests/unit/module/aggregate.spec.ts
+++ tests/unit/module/aggregate.spec.ts
@@ -3,6 +3,12 @@ const HORIZONTAL_VALUE = 4;
 const CHILD_COUNT = 2;
 const NODE_DEPTH = 4;
 
-const makeTESTCartHoldLog = () => ({});
+const makeTESTCartHoldLog = (value: number, rank: string): CartHoldLog => ({
+  name: `TEST${value}`,
+  childLogs: [] as CartHoldLog[],
+  imageurl: `TEST${value}.com`,
+  count: 1,
+  rank: `${rank}`,
+});
 
 describe("Module : 集計関数", () => {});
