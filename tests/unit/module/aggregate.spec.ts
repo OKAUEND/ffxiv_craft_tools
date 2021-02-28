c431839783711dc115ca22c627b2c6741e5e30dc
OKAUEND <def1f23sbb@gmail.com>
2021-03-01 08:20:11 +0900

    Add - [途中] 木構造のデータを自動的に用意したいためfactory関数を作る

--- tests/unit/module/aggregate.spec.ts
+++ tests/unit/module/aggregate.spec.ts
@@ -1,4 +1,6 @@
 import { findTreeTerminalLog } from "@/module/aggregate";
+import { CartHoldLog } from "@/@types/FFXIVLogTypes";
+
 const HORIZONTAL_VALUE = 4;
 const CHILD_COUNT = 2;
 const NODE_DEPTH = 4;
@@ -10,5 +12,17 @@ const makeTESTCartHoldLog = (value: number, rank: string): CartHoldLog => ({
   count: 1,
   rank: `${rank}`,
 });
+
+const makeTestLogFactory = () => {
+  const tops = [...Array(HORIZONTAL_VALUE)].map((count) => {
+    return makeTESTCartHoldLog(count, "TOP");
+  });
+
+  const middle = [...Array(HORIZONTAL_VALUE * CHILD_COUNT)].map((count) => {
+    const raw =
+    return makeTESTCartHoldLog(count, "middle");
+  });
+
+};
 
 describe("Module : 集計関数", () => {});
