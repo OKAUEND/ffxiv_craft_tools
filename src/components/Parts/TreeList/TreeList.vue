f9e6cc2ca8ca070adf325d398b5bfd660a7767d0
OKAUEND <def1f23sbb@gmail.com>
2021-05-14 04:49:38 +0900

    Add - ツリー構造で表示したいので、ネストさせる構成とする

--- src/components/Parts/TreeList/TreeList.vue
+++ src/components/Parts/TreeList/TreeList.vue
@@ -1,4 +1,20 @@
-<template>TEMP</template>
+<template>
+  <ul class="treelist">
+    <li class="treelist__parenttitle">
+      {{ props.Parent.name }}
+      <ul class="treelsit__list">
+        <li
+          v-for="item in treeChilds"
+          :key="item.order"
+          class="treelist__listitem"
+        >
+          <base-image-medium :src="item.url" />
+          {{ item.name }}
+        </li>
+      </ul>
+    </li>
+  </ul>
+</template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
