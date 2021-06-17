7eeacd510b7520cdea16cef716b436ffe3f37fd5
OKAUEND <def1f23sbb@gmail.com>
2021-06-17 23:46:17 +0900

    Change - 要素の構造を作成 - codepenで試したデザイン構造を反映する - BEM構造でクラスを実装

--- src/components/Parts/TreeList/TreeList.vue
+++ src/components/Parts/TreeList/TreeList.vue
@@ -1,15 +1,22 @@
 <template>
   <ul class="treelist">
-    <li class="treelist__parenttitle">
-      {{ props.Parent.name }}
+    <li class="treelist__parent">
+      <div class="treelist__body">
+        <img class="treelist__icon" />
+        {{ props.Parent.name }}
+        <div class="treelist__body--value">{{ props.Parent.value }}</div>
+      </div>
       <ul class="treelsit__list">
         <li
           v-for="item in treeChilds"
           :key="item.order"
-          class="treelist__listitem"
+          class="treelist__list-item"
         >
-          <base-image-medium :src="item.url" />
-          {{ item.name }}
+          <div class="treelist__body">
+            <base-image-medium :src="item.url" />
+            {{ item.name }}
+            <div class="treelist__body--value">{{ props.item.value }}</div>
+          </div>
         </li>
       </ul>
     </li>
