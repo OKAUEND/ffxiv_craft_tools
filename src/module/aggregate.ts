787529a2c2587488b060024b6cfec080a26e96ac
OKAUEND <def1f23sbb@gmail.com>
2021-07-03 02:46:45 +0900

    Change - 取得と新しいオブジェクト生成の2つをしていたので、役割分離をさせたいために変更

--- src/module/aggregate.ts
+++ src/module/aggregate.ts
@@ -11,29 +11,30 @@ import {
 } from "@/module/craftlogutiles";
 import { useProgress } from "@/module/statefull";
 
-export const validChildFactorOnlyEnable = (
-  childlogs: ChildLogs[]
-): ChildLogs[] => {
-  return childlogs.filter((childlog) => childlog.isEnable === true);
-};
+type ValidChildOnlyEnable = (a:SimpleCraftLogs[] ) => SimpleCraftLogs[]
 
-const createCartHoldLogs = async (craftlog: CraftLog) => {
-  const validChildFactor = validChildFactorOnlyEnable(craftlog.childrenlogs);
+const validChildOnlyEnable:ValidChildOnlyEnable = (param) =>{
+  return param.filter(item => item.isEnable === true)
+}
 
-  //子要素の子要素を取り出す
+const fetchAllValidChildFactor = async (craftlog: CraftLog) => {
+  //有効な子要素のみに絞り込む
+  const validChildFactor = validChildOnlyEnable(craftlog.simplechildlogs);
+
   return await Promise.all(
-    validChildFactor.map(
-      async (child): Promise<CartHoldLog> => {
-        //1つ1つの製作レシピをループで処理する
-        const log = await fetchCraftLogFromPath(child.childrenDocumentRef);
+    validChildFactor.map(async (child): Promise<CraftLog> => {
+      //1つ1つの製作レシピをループで処理する
+      const log = await fetchCraftLogFromPath(child.relationDocumentRef);
 
-        //ここで再帰処理を行い、さらに子のノードを探す
-        //親の製作個数を乗算しないようにしておき、元の製作個数を消失させないようにする
-        const childnode = await createCartHoldLogs(log);
+      //ここで再帰処理を行い、さらに子のノードを探す
+      //親の製作個数を乗算しないようにしておき、元の製作個数を消失させないようにする
+      const childnode = await fetchAllValidChildFactor(log);
 
-        return makeCartTempLogStructure(log, child.value, childnode);
-      }
-    )
+      //全データ版である素材情報の配列を専用のプロパティに入れる。
+      log.childlogs = childnode;
+      
+      return log;
+    })
   );
 };
 
